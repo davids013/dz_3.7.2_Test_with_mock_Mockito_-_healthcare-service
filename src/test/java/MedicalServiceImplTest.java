b950568b9693ba8a2583641d0e925a43482c22fa
Vitaly Shvedko <davids013013@gmail.com>
2021-08-31 00:07:58 +0300

    It works somehow. I hope so

--- src/test/java/MedicalServiceImplTest.java
+++ src/test/java/MedicalServiceImplTest.java
@@ -17,12 +17,13 @@ import java.io.PrintStream;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
 public class MedicalServiceImplTest {
     final String PATIENT_ID = "iddqd";
     final private String ALERT = String.format("Warning, patient with id: %s, need help", PATIENT_ID);
+    final private String WARNING = "Warning";
 
     @BeforeAll
     private static void start() { Methods.start(); }
@@ -37,74 +38,70 @@ public class MedicalServiceImplTest {
     private static void end() { Methods.end(); }
 
     @ParameterizedTest
-    @ValueSource(doubles = {33.5, 35.1, 36.8, 38.0, 39.7})
+    @ValueSource(strings = {"33.5", "35.1", "36.8", "38.0", "39.7"})
     void checkTemperatureTest(double param) {
-        ByteArrayOutputStream output = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(output));
-        final BigDecimal normalTemperature = new BigDecimal(37.0);
+        final BigDecimal normalTemperature = new BigDecimal("37.0");
         final BigDecimal patientTemperature = new BigDecimal(param);
-        final PatientInfoRepository patientInfoRepository = mock(PatientInfoFileRepository.class);
-        final HealthInfo healthInfo = mock(HealthInfo.class);
-        final SendAlertService sendAlertService = mock(SendAlertServiceImpl.class);
-        final PatientInfo patientInfo =
-                new PatientInfo(PATIENT_ID, "John", "Doe", LocalDate.now(), healthInfo);
-        when(healthInfo.getNormalTemperature()).thenReturn(normalTemperature);
-        when(patientInfoRepository.getById(PATIENT_ID)).thenReturn(patientInfo);
+        final ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
         final String expected =
                 (normalTemperature
                         .subtract(new BigDecimal("1.5"))
                         .compareTo(patientTemperature) > 0)
-                        ? ALERT
+                        ? WARNING
                         : "";
         try {
             output.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+        final PatientInfoRepository patientInfoRepository = mock(PatientInfoFileRepository.class);
+        final HealthInfo healthInfo = mock(HealthInfo.class);
+        final SendAlertService sendAlertService = mock(SendAlertServiceImpl.class);
+        final PatientInfo patientInfo = mock(PatientInfo.class);
+        when(patientInfo.getHealthInfo()).thenReturn(healthInfo);
+        when(healthInfo.getNormalTemperature()).thenReturn(normalTemperature);
+        when(patientInfoRepository.getById(PATIENT_ID)).thenReturn(patientInfo);
 
-        MedicalService medicalService = new MedicalServiceImpl(patientInfoRepository, sendAlertService);
+
+        final MedicalService medicalService = new MedicalServiceImpl(patientInfoRepository, sendAlertService);
         medicalService.checkTemperature(PATIENT_ID, patientTemperature);
-        assertEquals(expected, output.toString());
+        final String result = output.toString();
+        assertTrue(result.startsWith(expected));
     }
 
-    @Test
-    void checkBloodPressureTest() {
-        final BloodPressure patientPressure = new BloodPressure(180, 90);
-        final BloodPressure bloodPressure = new BloodPressure(180, 90);
-        //String alert = "Achtung!";
+    @ParameterizedTest
+    @ValueSource(ints = {100, 120, 150, 200})
+    void checkBloodPressureTest(int param) {
+        final BloodPressure patientPressure = new BloodPressure(param, 90);
+        final BloodPressure bloodPressure = new BloodPressure(120, 80);
+        final ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+        String expected = (!patientPressure.equals(bloodPressure)) ? WARNING : "";
+        try {
+            output.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         final PatientInfoRepository patientInfoRepository = mock(PatientInfoFileRepository.class);
-        final HealthInfo healthInfo =
-                mock(HealthInfo.class);
-//                new HealthInfo(new BigDecimal(36.6), new BloodPressure(120, 70));
+        final HealthInfo healthInfo = mock(HealthInfo.class);
         final PatientInfo patientInfo = mock(PatientInfo.class);
-//                new PatientInfo(
-//                PATIENT_ID, "John", "Doe", LocalDate.now(), healthInfo);
         when(patientInfo.getHealthInfo()).thenReturn(healthInfo);
         when(patientInfo.getHealthInfo().getBloodPressure()).thenReturn(bloodPressure);
-        //when(healthInfo.getBloodPressure()).thenReturn(bloodPressure);
-//        when(patientInfoRepository.getById(PATIENT_ID)).thenReturn(patientInfo);
         when(patientInfoRepository.getById(PATIENT_ID))
                 .thenReturn(new PatientInfo(PATIENT_ID, "John", "Doe", LocalDate.now(),
-                        new HealthInfo(new BigDecimal(36.6), patientPressure)));
-        SendAlertService sendAlertService =
-//                new SendAlertServiceImpl();
-                mock(SendAlertServiceImpl.class);
+                        new HealthInfo(new BigDecimal("36.6"), patientPressure)));
+        final SendAlertService sendAlertService = mock(SendAlertServiceImpl.class);
         doCallRealMethod().when(sendAlertService).send(ALERT);
 //        ArgumentCaptor<String> argumentCaptor = ArgumentCaptor.forClass(String.class);
 
 
-        MedicalService medicalService = new MedicalServiceImpl(patientInfoRepository, sendAlertService);
+        final MedicalService medicalService = new MedicalServiceImpl(patientInfoRepository, sendAlertService);
 //        Mockito.verify(sendAlertService.send(argumentCaptor.capture()));
 //        Mockito.verify(sendAlertService).send(argumentCaptor.capture());
 //        System.out.println(argumentCaptor.getValue());
         medicalService.checkBloodPressure(PATIENT_ID, bloodPressure);
-    }
-
-    @Test
-    void getPatientInfoTest() {
-        PatientInfoRepository patientInfoRepository = mock(PatientInfoFileRepository.class);
-        when(patientInfoRepository.getById(PATIENT_ID))
-                .thenReturn(new PatientInfo(
-                        PATIENT_ID, "John", "Doe", LocalDate.now(), new HealthInfo()));
+        final String result = output.toString();
+        assertTrue(result.startsWith(expected));
     }
 }
