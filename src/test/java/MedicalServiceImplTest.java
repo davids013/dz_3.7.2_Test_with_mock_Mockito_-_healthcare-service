52e92a1f27f9b1c666e0d3475f7cd1311fb74eef
Vitaly Shvedko <davids013013@gmail.com>
2021-08-31 22:02:01 +0300

    All done

--- src/test/java/MedicalServiceImplTest.java
+++ src/test/java/MedicalServiceImplTest.java
@@ -1,7 +1,8 @@
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.mockito.Mockito;
+import org.mockito.ArgumentCaptor;
 import ru.netology.patient.entity.BloodPressure;
 import ru.netology.patient.entity.HealthInfo;
 import ru.netology.patient.entity.PatientInfo;
@@ -16,15 +17,14 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.PrintStream;
 import java.math.BigDecimal;
-import java.time.LocalDate;
+import java.util.stream.Stream;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
 public class MedicalServiceImplTest {
     final String PATIENT_ID = "iddqd";
-    final private String ALERT = String.format("Warning, patient with id: %s, need help", PATIENT_ID);
-    final private String WARNING = "Warning";
 
     @BeforeAll
     private static void start() { Methods.start(); }
@@ -44,6 +44,7 @@ public class MedicalServiceImplTest {
         final BigDecimal normalTemperature = new BigDecimal("37.0");
         final BigDecimal patientTemperature = new BigDecimal(param);
         final ByteArrayOutputStream output = new ByteArrayOutputStream();
+        final String WARNING = "Warning";
         System.setOut(new PrintStream(output));
         final String expected =
                 (normalTemperature
@@ -56,52 +57,54 @@ public class MedicalServiceImplTest {
         } catch (IOException e) {
             e.printStackTrace();
         }
-        final PatientInfoRepository patientInfoRepository = mock(PatientInfoFileRepository.class);
+        final PatientInfoRepository patientInfoRepository =
+                mock(PatientInfoFileRepository.class);
         final HealthInfo healthInfo = mock(HealthInfo.class);
         final SendAlertService sendAlertService = mock(SendAlertServiceImpl.class);
         final PatientInfo patientInfo = mock(PatientInfo.class);
         when(patientInfo.getHealthInfo()).thenReturn(healthInfo);
         when(healthInfo.getNormalTemperature()).thenReturn(normalTemperature);
         when(patientInfoRepository.getById(PATIENT_ID)).thenReturn(patientInfo);
+        final ArgumentCaptor<String> argumentCaptor = ArgumentCaptor.forClass(String.class);
 
-        final MedicalService medicalService = new MedicalServiceImpl(patientInfoRepository, sendAlertService);
+        final MedicalService medicalService =
+                new MedicalServiceImpl(patientInfoRepository, sendAlertService);
         medicalService.checkTemperature(PATIENT_ID, patientTemperature);
+        verify(healthInfo, times(1)).getNormalTemperature();
+        verify(patientInfoRepository, only()).getById(argumentCaptor.capture());
         final String result = output.toString();
+        assertEquals(PATIENT_ID, argumentCaptor.getValue());
         assertTrue(result.startsWith(expected));
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {100, 120, 150, 200})
+    @MethodSource("getIntegers")
     void checkBloodPressureTest(int param) {
         final BloodPressure patientPressure = new BloodPressure(param, 80);
         final BloodPressure bloodPressure = new BloodPressure(120, 80);
-        final ByteArrayOutputStream output = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(output));
-        String expected = (!patientPressure.equals(bloodPressure)) ? WARNING : "";
-        try {
-            output.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        final PatientInfoRepository patientInfoRepository = mock(PatientInfoFileRepository.class);
+        final PatientInfoRepository patientInfoRepository =
+                mock(PatientInfoFileRepository.class);
         final HealthInfo healthInfo = mock(HealthInfo.class);
         final PatientInfo patientInfo = mock(PatientInfo.class);
-        when(patientInfo.getHealthInfo()).thenReturn(healthInfo);
-        when(patientInfo.getHealthInfo().getBloodPressure()).thenReturn(bloodPressure);
-        when(patientInfoRepository.getById(PATIENT_ID))
-                .thenReturn(new PatientInfo(PATIENT_ID, "John", "Doe", LocalDate.now(),
-                        new HealthInfo(new BigDecimal("36.6"), patientPressure)));
         final SendAlertService sendAlertService = mock(SendAlertServiceImpl.class);
-        doCallRealMethod().when(sendAlertService).send(ALERT);
-//        ArgumentCaptor<String> argumentCaptor = ArgumentCaptor.forClass(String.class);
+        when(patientInfoRepository.getById(any())).thenReturn(patientInfo);
+        when(patientInfo.getHealthInfo()).thenReturn(healthInfo);
+        when(healthInfo.getBloodPressure()).thenReturn(patientPressure);
+        doCallRealMethod().when(sendAlertService).send(any());
+        final ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
 
-
-        final MedicalService medicalService = new MedicalServiceImpl(patientInfoRepository, sendAlertService);
-//        Mockito.verify(sendAlertService.send(argumentCaptor.capture()));
-//        Mockito.verify(sendAlertService).send(argumentCaptor.capture());
-//        System.out.println(argumentCaptor.getValue());
+        final MedicalService medicalService =
+                new MedicalServiceImpl(patientInfoRepository, sendAlertService);
         medicalService.checkBloodPressure(PATIENT_ID, bloodPressure);
-        final String result = output.toString();
-        assertTrue(result.startsWith(expected));
+        verify(patientInfoRepository).getById(captor.capture());
+        assertEquals(PATIENT_ID, captor.getValue());
+        verify(sendAlertService,
+                times((patientPressure.equals(bloodPressure)) ? 0 : 1)).send(any());
+    }
+
+    private static Stream<Integer> getIntegers() {
+        return Stream
+                .iterate(80, n -> n + 20)
+                .limit(5);
     }
 }
